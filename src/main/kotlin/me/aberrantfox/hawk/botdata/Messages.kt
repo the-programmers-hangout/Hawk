012379fbcdf842b3d0058b50d329f3a037f9e6ab
Aberrantfox <aberrantfoxtph@gmail.com>
2020-01-11 15:48:44 +0000

    Rename to name that doesn't conflict with .gitignore

---
+++ src/main/kotlin/me/aberrantfox/hawk/botdata/Messages.kt
@@ -0,0 +1,9 @@
+package me.aberrantfox.hawk.botdata
+
+import me.aberrantfox.kjdautils.api.annotation.Data
+
+@Data("data/messages.json")
+data class Messages(
+    val USER_NICK_RESET: String = "Your nickname made it seem like you were a staff member. This has been automatically logged.",
+    val STAFF_NICK_PROMPT: String = "You have updated your nickname and it does not contain the prefix. Here it is with the prefix: %1%"
+)
