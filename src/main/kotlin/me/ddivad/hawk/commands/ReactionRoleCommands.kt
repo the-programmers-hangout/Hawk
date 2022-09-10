f1f63a1eba3ae48cabe104aed57aa1f5a4c8eead
ddivad195 <ddivad195@gmail.com>
2022-09-10 13:23:32 +0100

    feat: error checking for role ids

--- src/main/kotlin/me/ddivad/hawk/commands/ReactionRoleCommands.kt
+++ src/main/kotlin/me/ddivad/hawk/commands/ReactionRoleCommands.kt
@@ -5,10 +5,14 @@ import me.ddivad.hawk.dataclasses.Permissions
 import me.ddivad.hawk.dataclasses.ReactionRole
 import me.ddivad.hawk.embeds.createReactionRoleMenu
 import me.ddivad.hawk.services.LoggingService
+import me.ddivad.hawk.services.buildGuildLogMessage
 import me.jakejmattson.discordkt.arguments.EveryArg
 import me.jakejmattson.discordkt.commands.commands
 import me.jakejmattson.discordkt.dsl.edit
 import me.jakejmattson.discordkt.extensions.toSnowflake
+import mu.KotlinLogging
+
+val logger = KotlinLogging.logger { }
 
 @Suppress("unused")
 fun reactionRoleCommands(configuration: Configuration, loggingService: LoggingService) = commands("ReactionRole") {
@@ -18,22 +22,27 @@ fun reactionRoleCommands(configuration: Configuration, loggingService: LoggingSe
             EveryArg("EmbedDescription", "Text to be added to reaction role embed")
         ) {
             val (roleIds, descriptionText) = args
-            val roles = roleIds.split(" ").map { guild.getRole(it.toSnowflake()) }
-            val guildConfig = configuration[guild.id] ?: return@execute
-            val reactionRole = ReactionRole(
-                guildConfig.reactionRoles.size + 1,
-                descriptionText,
-                roles.map { it.id }.toMutableList(),
-                null,
-                channel.id
-            )
-            reactionRole.messageId = respondMenu {
-                createReactionRoleMenu(discord, guild, reactionRole)
-            }.id
-            configuration.edit {
-                guildConfig.reactionRoles.add(reactionRole)
+            try {
+                val roles = roleIds.split(" ").map { guild.getRole(it.toSnowflake()) }
+                val guildConfig = configuration[guild.id] ?: return@execute
+                val reactionRole = ReactionRole(
+                    guildConfig.reactionRoles.size + 1,
+                    descriptionText,
+                    roles.map { it.id }.toMutableList(),
+                    null,
+                    channel.id
+                )
+                reactionRole.messageId = respondMenu {
+                    createReactionRoleMenu(discord, guild, reactionRole)
+                }.id
+                configuration.edit {
+                    guildConfig.reactionRoles.add(reactionRole)
+                }
+                respond("Reaction role created")
+            } catch (e: Exception) {
+                respond("Error parsing roles. Make sure IDs $roleIds are valid roles and separated by ` ` if adding multiple roles.")
+                logger.error(e) { buildGuildLogMessage(guild, "Failed to parse roles") }
             }
-            respond("Reaction role created")
         }
     }
 }
