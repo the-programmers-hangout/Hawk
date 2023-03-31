81d41f6453289d0f5553bc3cfc170a21263f52cd
ddivad195 <ddivad195@gmail.com>
2023-04-01 00:41:34 +0100

    feat: add nickname themes

--- src/main/kotlin/me/ddivad/hawk/commands/NicknameCommands.kt
+++ src/main/kotlin/me/ddivad/hawk/commands/NicknameCommands.kt
@@ -4,6 +4,7 @@ import dev.kord.core.behavior.edit
 import me.ddivad.hawk.arguments.LowerMemberArg
 import me.ddivad.hawk.dataclasses.Configuration
 import me.ddivad.hawk.services.LoggingService
+import me.ddivad.hawk.services.NicknameService
 import me.jakejmattson.discordkt.arguments.AnyArg
 import me.jakejmattson.discordkt.arguments.ChoiceArg
 import me.jakejmattson.discordkt.arguments.EveryArg
@@ -11,18 +12,27 @@ import me.jakejmattson.discordkt.commands.commands
 import me.jakejmattson.discordkt.extensions.addField
 
 @Suppress("unused")
-fun nicknameCommands(configuration: Configuration, loggingService: LoggingService
+fun nicknameCommands(configuration: Configuration, loggingService: LoggingService, nicknameService: NicknameService
 ) = commands("Nickname") {
     slash("nick", "Set a member's nickname") {
-        execute(LowerMemberArg, EveryArg("Nickname")) {
-            val (member, nickname) = args
-            if (nickname.length > 32) {
+        execute(LowerMemberArg, ChoiceArg("Theme", "Choose a theme for nicknames", "Manual", "Furry"), EveryArg("Nickname", "Optional Nickname for use with Manual option").optionalNullable(null) ) {
+            val (member, theme, nickname) = args
+            if ((nickname?.length ?: 0) > 32) {
                 respond("Nickname needs to be < 32 characters")
                 return@execute
             }
-            member.edit { this.nickname = nickname }
-            respond("Nickname set to **$nickname**")
-            loggingService.nicknameApplied(guild, member, nickname)
+
+            val newNickname = when (theme) {
+                "Manual" -> nickname
+                "Furry" -> nicknameService.furryNames.random()
+                else -> null
+            }
+
+            newNickname?.let {
+                member.edit { this.nickname = newNickname }
+                loggingService.nicknameApplied(guild, member, newNickname)
+            }
+            respond("Nickname set to **$newNickname**")
         }
     }
 
