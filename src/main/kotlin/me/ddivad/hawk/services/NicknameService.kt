686858907a87d82aed27db18109078578b79d6f4
ddivad195 <ddivad195@gmail.com>
2023-04-01 02:11:15 +0100

    fix: party mode bugfixes

--- src/main/kotlin/me/ddivad/hawk/services/NicknameService.kt
+++ src/main/kotlin/me/ddivad/hawk/services/NicknameService.kt
@@ -11,33 +11,39 @@ import me.jakejmattson.discordkt.annotations.Service
 class NicknameService(private val configuration: Configuration, private val loggingService: LoggingService) {
     suspend fun setOrRemovePartyNickname(guild: Guild, member: Member, channel: TextChannel) {
         val partyConfiguration = configuration[guild.id]?.partyModeConfiguration ?: return
-        if(!partyConfiguration.enabled) {
-            return
-        }
         if (member.isBot) {
             return
         }
         if (partyConfiguration.channelFilterEnabled && !partyConfiguration.channels.contains(channel.id)) {
             return
         }
-        if (partyConfiguration.mode == "Furry" && !furryNames.contains(member.displayName)) {
-            println(member.displayName)
-            val nickname = furryNames.random()
-            changeMemberNickname(guild, member, nickname)
-            return
-        }
-        if (partyConfiguration.symbol.isNullOrBlank()) {
-            return
+        if (partyConfiguration.mode == "Symbol") {
+            if (partyConfiguration.symbol.isBlank()) {
+                return
+            }
+            val hasPartyNicknameApplied =
+                member.displayName.endsWith(partyConfiguration.symbol) || member.displayName.endsWith(partyConfiguration.symbolStrip)
+            if (!hasPartyNicknameApplied && partyConfiguration.enabled) {
+                val nickname = addNickSymbol(member.displayName, partyConfiguration.symbol, partyConfiguration.symbolStrip)
+                changeMemberNickname(guild, member, nickname)
+            } else if (hasPartyNicknameApplied && !partyConfiguration.enabled) {
+                val nickname = removeNickSymbol(member.displayName, mutableListOf(partyConfiguration.symbolStrip))
+                changeMemberNickname(guild, member, nickname)
+            }
         }
 
-        val hasPartyNicknameApplied =
-            member.displayName.endsWith(partyConfiguration.symbol) || member.displayName.endsWith(partyConfiguration.symbolStrip)
-        if (!hasPartyNicknameApplied && partyConfiguration.enabled) {
-            val nickname = addNickSymbol(member.displayName, partyConfiguration.symbol, partyConfiguration.symbolStrip)
+        if (partyConfiguration.mode == "Furry" && !furryNames.containsAll(member.displayName.split(" "))) {
+            if (!partyConfiguration.enabled) {
+                return
+            }
+            var firstPart = "${furryNames.random()}"
+            var secondPart = "${furryNames.random()}"
+            while (firstPart == secondPart) {
+                secondPart = "${furryNames.random()}"
+            }
+            val nickname = "$firstPart $secondPart"
             changeMemberNickname(guild, member, nickname)
-        } else if (hasPartyNicknameApplied && !partyConfiguration.enabled) {
-            val nickname = removeNickSymbol(member.displayName, mutableListOf(partyConfiguration.symbolStrip))
-            changeMemberNickname(guild, member, nickname)
+            return
         }
     }
 
@@ -89,144 +95,91 @@ class NicknameService(private val configuration: Configuration, private val logg
     }
 
     val furryNames = listOf(
-        "Shadowfang", "Thunderstrike",
-        "Sunnywhisker", "Briskclaw",
-        "Moonclaw", "Silvershadow",
-        "Dusksnout", "Wittytail",
-        "Frostyclaw Luckyeyes",
-        "Copperpelt Softfur",
-        "Silentwhisker Darkmug",
-        "Rainwhisker Cleverheart",
-        "Amberpaw Boldshadow",
-        "Mistycoat Rapidfeet",
-        "Sneakyclaw Darksnout",
-        "Sparklingfur Sharpclaw",
-        "Ravencrown Luckytail",
-        "Thunderwhisker Softyelp",
-        "Snowyclaw Swiftfeet",
-        "Crystalpelt Fierceheart",
-        "Flamepaw Brightclaw",
-        "Leafcoat Jollytooth",
-        "Stormyclaw Wildyelp",
-        "Emberwhisker Humblepelt",
-        "Smokyfur Richmug",
-        "Goldenclaw Silenttail",
-        "Mistyclaw Calmheart",
-        "Midnightfur Snowytooth",
-        "Honeywhisker Lazytail",
-        "Sablepelt Darkclaw",
-        "Braveclaw Swiftmug",
-        "Fuzzywhisker Braveheart",
-        "Dustyclaw Rustytail",
-        "Thundercoat Calmfeet",
-        "Snowpaw Coldyelp",
-        "Fluffywhisker Fierceclaw",
-        "Dawnfur Quickheart",
-        "Nightclaw Wiseeyes",
-        "Rapidclaw Sillytail",
-        "Mistywhisker Richshadow",
-        "Gentlecoat Swiftmug",
-        "Brightclaw Windyheart",
-        "Sneakypelt Strongclaw",
-        "Twilightfur Wiseyelp",
-        "Majesticpaw Darkfeather",
-        "Snowpelt Coldtoes",
-        "Windyfur Softyelp",
-        "Stormyclaw Humbleheart",
-        "Tundrapelt Speedytooth",
-        "Breezywhisker Rustymug",
-        "Goldenwhisker Jollyclaw",
-        "Sandycoat Boldyelp",
-        "Lunarclaw Richpelt",
-        "Sablewhisker Quicktail",
-        "Brightcoat Braveheart",
-        "Crimsonpaw Wittyheart",
-        "Thunderpelt Fierceclaw",
-        "Cloudyclaw Silentmug",
-        "Swiftcoat Windytooth",
-        "Midnightwhisker Sillyclaw",
-        "Smokyfur Humblepelt",
-        "Silverclaw Darkfeather",
-        "Mistywhisker Swifttail",
-        "Duskycoat Richyelp",
-        "Crystalpelt Speedyclaw",
-        "Fluffywhisker Darkheart",
-        "Ravencrown Softclaw",
-        "Thunderpaw Snowytail",
-        "Dawnclaw Rustyheart",
-        "Mistyclaw Wildclaw",
-        "Gentlewhisker Boldtooth",
-        "Sablepaw Softtail",
-        "Majesticfur Rustyeyes",
-        "Frostclaw Coolpaws",
-        "Moonwhisker Twinkletoes",
-        "Sablefur Wildheart",
-        "Spirithawk Gloomyswoop",
-        "Thunderfox Mightyfang",
-        "Mistysnout Dreamyhowl",
-        "Goldenpelt Richmane",
-        "Fireflyer Quickwings",
-        "Swiftbunny Furryfeet",
-        "Wildwhisker Roamingeyes",
-        "Mudpaw Grittyclaw",
-        "Stonefang Hardhide",
-        "Cloudchaser Fluffytail",
-        "Leafylynx Sleekears",
-        "Blazingtiger Fiercegrowl",
-        "Junglejaguar Sneakypaws",
-        "Silverswift Swiftclaw",
-        "Thornystud Proudmane",
-        "Smokysnout Greyhowl",
-        "Riverscale Riverheart",
-        "Stormysky Thunderpaws",
-        "Aurora",
-        "Blaze",
-        "Crimson",
-        "Dakota",
-        "Echo",
-        "Flame",
-        "Ginger",
-        "Harmony",
-        "Indigo",
-        "Jasper",
-        "Koda",
-        "Luna",
-        "Midnight",
-        "Nova",
-        "Onyx",
-        "Phoenix",
-        "Quinn",
-        "Raven",
-        "Sable",
-        "Shadow",
-        "Sky",
-        "Sonic",
-        "Storm",
-        "Talon",
-        "Topaz",
-        "Violet",
-        "Willow",
-        "Wolfie",
-        "Zephyr",
-        "Amber",
-        "Bolt",
-        "Chase",
-        "Dante",
-        "Ember",
-        "Fawn",
-        "Gadget",
-        "Hawk",
-        "Ivy",
-        "Jade",
-        "Kai",
-        "Loki",
-        "Misty",
-        "Nero",
-        "Orion",
-        "Piper",
-        "Riley",
-        "Sapphire",
-        "Smokey",
-        "Tiger",
+        "Frostyclaw", "Luckyeyes",
+        "Copperpelt", "Softfur",
+        "Silentwhisker", "Darkmug",
+        "Rainwhisker", "Cleverheart",
+        "Amberpaw", "Boldshadow",
+        "Mistycoat", "Rapidfeet",
+        "Sneakyclaw", "Darksnout",
+        "Sparklingfur", "Sharpclaw",
+        "Ravencrown", "Luckytail",
+        "Thunderwhisker", "Softyelp",
+        "Snowyclaw", "Swiftfeet",
+        "Crystalpelt", "Fierceheart",
+        "Flamepaw", "Brightclaw",
+        "Leafcoat", "Jollytooth",
+        "Stormyclaw", "Wildyelp",
+        "Emberwhisker", "Humblepelt",
+        "Smokyfur", "Richmug",
+        "Goldenclaw", "Silenttail",
+        "Mistyclaw", "Calmheart",
+        "Midnightfur", "Snowytooth",
+        "Honeywhisker", "Lazytail",
+        "Sablepelt", "Darkclaw",
+        "Braveclaw", "Swiftmug",
+        "Fuzzywhisker", "Braveheart",
+        "Dustyclaw", "Rustytail",
+        "Thundercoat", "Calmfeet",
+        "Snowpaw", "Coldyelp",
+        "Fluffywhisker", "Fierceclaw",
+        "Dawnfur", "Quickheart",
+        "Nightclaw", "Wiseeyes",
+        "Rapidclaw", "Sillytail",
+        "Mistywhisker", "Richshadow",
+        "Gentlecoat", "Swiftmug",
+        "Brightclaw", "Windyheart",
+        "Sneakypelt", "Strongclaw",
+        "Twilightfur", "Wiseyelp",
+        "Majesticpaw", "Darkfeather",
+        "Snowpelt", "Coldtoes",
+        "Windyfur", "Softyelp",
+        "Stormyclaw", "Humbleheart",
+        "Tundrapelt", "Speedytooth",
+        "Breezywhisker", "Rustymug",
+        "Goldenwhisker", "Jollyclaw",
+        "Sandycoat", "Boldyelp",
+        "Lunarclaw", "Richpelt",
+        "Sablewhisker", "Quicktail",
+        "Brightcoat", "Braveheart",
+        "Crimsonpaw", "Wittyheart",
+        "Thunderpelt", "Fierceclaw",
+        "Cloudyclaw", "Silentmug",
+        "Swiftcoat", "Windytooth",
+        "Midnightwhisker", "Sillyclaw",
+        "Smokyfur", "Humblepelt",
+        "Silverclaw", "Darkfeather",
+        "Mistywhisker", "Swifttail",
+        "Duskycoat", "Richyelp",
+        "Crystalpelt", "Speedyclaw",
+        "Fluffywhisker", "Darkheart",
+        "Ravencrown", "Softclaw",
+        "Thunderpaw", "Snowytail",
+        "Dawnclaw", "Rustyheart",
+        "Mistyclaw", "Wildclaw",
+        "Gentlewhisker", "Boldtooth",
+        "Sablepaw", "Softtail",
+        "Majesticfur", "Rustyeyes",
+        "Frostclaw", "Coolpaws",
+        "Moonwhisker", "Twinkletoes",
+        "Sablefur", "Wildheart",
+        "Spirithawk", "Gloomyswoop",
+        "Thunderfox", "Mightyfang",
+        "Mistysnout", "Dreamyhowl",
+        "Goldenpelt", "Richmane",
+        "Fireflyer", "Quickwings",
+        "Swiftbunny", "Furryfeet",
+        "Wildwhisker", "Roamingeyes",
+        "Mudpaw", "Grittyclaw",
+        "Stonefang", "Hardhide",
+        "Cloudchaser", "Fluffytail",
+        "Leafylynx", "Sleekears",
+        "Blazingtiger", "Fiercegrowl",
+        "Junglejaguar", "Sneakypaws",
+        "Silverswift", "Swiftclaw",
+        "Thornystud", "Proudmane",
+        "Smokysnout", "Greyhowl",
+        "Riverscale", "Riverheart",
+        "Stormysky", "Thunderpaws",
     )
 }
